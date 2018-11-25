687897ace16525d693b56f384731243291c085e1
Paul Krenn <paul.krenn@lab73.at>
2018-11-25 18:06:43 +0100

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Paul Krenn on 25.11.18.
+//  Copyright © 2018 Paul Krenn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
